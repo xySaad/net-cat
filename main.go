d5aa1fba4d9218da8ad45054c2d0b5400b39836d
srm <saadrm.dev@gmail.com>
2024-11-02 09:10:58 +0100

    handle port argument

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 
 var (
 	Address = "0.0.0.0"
-	Port    = "2000"
+	Port    = "8989"
 )
 
 func main() {
+	if len(os.Args) > 2 {
+		fmt.Fprintln(os.Stderr, "too many arguments")
+		return
+	}
+	if len(os.Args) == 2 {
+		Port = os.Args[1]
+	}
+
 	adress := Address + ":" + Port
 	err := server.Run(adress)
 	if err != nil {
