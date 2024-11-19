4457d7aece9771525d620441ee77f07a3ffa4ae2
srm <saadrm.dev@gmail.com>
2024-11-19 10:29:32 +0100

    fix: correct a typo

--- server/modules/commands.go
+++ server/modules/commands.go
@@ -4,22 +4,22 @@ import "net-cat/utils"
 
 const CommandList = "\033[1m\033[32mCTRL + L ===> clear the page\nCTRL + N  ===> option to change your name\nCTRL + H ===> shows all available comands \nCTRL + O ==> shows all online members in the group\nCTRL + E ===> restore chat\n\033[0m"
 
-type commandsMap map[uint8]func(conn *Connection)
+type commandsMap map[uint8]func(conn *User)
 
 var Comands = commandsMap{
-	'H': func(conn *Connection) {
+	'H': func(conn *User) {
 		conn.Write([]byte(CommandList))
 		conn.Write([]byte(utils.GetPrefix(conn.UserName)))
 	},
-	'L': func(conn *Connection) {
+	'L': func(conn *User) {
 		conn.Write([]byte("\033[2J\033[3J\033[H"))
 		conn.Write([]byte(utils.GetPrefix(conn.UserName)))
 	},
-	'E': func(conn *Connection) {
+	'E': func(conn *User) {
 		conn.Write([]byte("\033[2J\033[3J\033[H"))
 		conn.RestoreHistory()
 	},
-	'O': func(conn *Connection) {
+	'O': func(conn *User) {
 		groupMembers := ""
 		for member := range Groups.List[conn.GroupName] {
 			groupMembers += member + "\n"
